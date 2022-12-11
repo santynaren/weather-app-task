0644839309ed9b4a89e6901475307df3b11af438
santynaren <santhoshnarendra@gmail.com>
2022-12-12 00:17:41 +0530

    feat: add logic to update current location and get city value

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ type CoOrdinateProps = {
 }
 function App() {
   const [coords, setCoords] = useState<CoOrdinateProps>({ lat: 0, lng: 0 })
+  const [city, setCity] = useState<string>("");
   useEffect(() => {
     // Following gets the lat lng for the user location
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -15,10 +16,21 @@ function App() {
       })
     });
   }, [])
-  console.log(coords)
+  useEffect(() => {
+    updateLocation();
+  })
+  const updateLocation = () => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&result_type=administrative_area_level_3&key=AIzaSyDt_i7_mJ0RhPZUTTF2L38jaewS8fU77dg`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json.results[0].formatted_address)
+        setCity(json.results[0].formatted_address)
+      })
+  }
   return (
     <div>
-      {/* App here */}
+{city}
     </div>
   );
 }
