0920718026fcf7514f92855a6e199f14139e697e
santynaren <santhoshnarendra@gmail.com>
2022-12-12 00:12:17 +0530

    feat: add logic to get current location

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
+type CoOrdinateProps = {
+  lat: number;
+  lng: number;
+}
 function App() {
+  const [coords, setCoords] = useState<CoOrdinateProps>({ lat: 0, lng: 0 })
+  useEffect(() => {
+    // Following gets the lat lng for the user location
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setCoords({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      })
+    });
+  }, [])
+  console.log(coords)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* App here */}
     </div>
   );
 }
