c02c3beba79c3616daf4cd9adcc3cc881339ac7e
santynaren <santhoshnarendra@gmail.com>
2022-12-12 00:19:27 +0530

    chore: remove logs

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,6 @@ function App() {
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&result_type=administrative_area_level_3&key=AIzaSyDt_i7_mJ0RhPZUTTF2L38jaewS8fU77dg`)
       .then((res) => res.json())
       .then((json) => {
-        console.log(json.results[0].formatted_address)
         setCity(json.results[0].formatted_address)
       })
   }
@@ -36,7 +35,6 @@ function App() {
       `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lng}&appid=1510748ac25b198ecae2f95bd9acc5d6`)
       .then((res) => res.json())
       .then((json) => {
-        console.log(json)
         setWeatherDetails((json.main.temp - 273.15).toFixed(0))
       })
   }
